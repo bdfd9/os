2010036dc85b3d2999be1e41b804beb2cefcda36
bdfd9 <dpaprocki+github@proton.me>
2026-08-28 19:26:40 +0200

    wip

--- files/scripts/common/harden-login-defs.sh
+++ files/scripts/common/harden-login-defs.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 PATCH_ARGS=("--forward" "--strip=1" "--no-backup-if-mismatch")
 
-patch /etc/login.defs "${PATCH_ARGS[@]}" < harden-login-defs.patch
+patch /etc/login.defs "${PATCH_ARGS[@]}" < common/harden-login-defs.patch
