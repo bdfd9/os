f7588bdb4bae717457bc3f587ceb5f899440607e
bdfd9 <dpaprocki+github@proton.me>
2026-09-06 13:36:48 +0200

    wip

--- files/scripts/mchose-battery/install.sh
+++ files/scripts/mchose-battery/install.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 
 KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-curl -fLsS --retry 5 https://github.com/bdfd9/mchose-battery/archive/refs/tags/v1.0.0.tar.gz
+wget --tries=5 https://github.com/bdfd9/mchose-battery/archive/refs/tags/v1.0.0.tar.gz
 
 tar -z -x --no-same-owner --no-same-permissions -f "v1.0.0.tar.gz"
 
