1dd75459371786f5ee8fb6eb06bb0d27ee0d1882
bdfd9 <dpaprocki+github@proton.me>
2026-09-06 13:10:50 +0200

    add mbat

---
+++ files/scripts/mchose-battery/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
+curl -fLsS --retry 5 https://github.com/bdfd9/mchose-battery/archive/refs/tags/v1.0.0.tar.gz
+
+tar -z -x --no-same-owner --no-same-permissions -f "v1.0.0.tar.gz"
+
+cd mchose-battery-1.0.0
+
+make install
+
+depmod -a -v "${KERNEL_VERSION}"
