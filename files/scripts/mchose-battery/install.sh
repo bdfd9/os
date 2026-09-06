303f1e5f66b9e4fbc605f5d9f8ba748235c4211b
bdfd9 <dpaprocki+github@proton.me>
2026-09-06 13:56:37 +0200

    wip

--- files/scripts/mchose-battery/install.sh
+++ files/scripts/mchose-battery/install.sh
@@ -3,6 +3,7 @@
 set -euox pipefail
 
 KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+dnf install -y --setopt=install_weak_deps=False "kernel-devel-matched-${KERNEL_VERSION}"
 
 wget --tries=5 https://github.com/bdfd9/mchose-battery/archive/refs/tags/v1.0.0.tar.gz
 
