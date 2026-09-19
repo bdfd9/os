9df4e518927e933d412e601d811a34351c93f55c
bdfd9 <dpaprocki+github@proton.me>
2026-09-19 18:13:06 +0200

    wip

--- files/scripts/45-drives/install-python3-libzfs.sh
+++ files/scripts/45-drives/install-python3-libzfs.sh
@@ -2,14 +2,26 @@
 
 set -euox pipefail
 
+tmp_dir=$(mktemp -d)
+
+function cleanup() {
+    rm -rf "${tmp_dir}"
+    dnf remove -y python3-cython
+    exit
+}
+
+trap cleanup ERR EXIT
+
+cd "${tmp_dir}"
+
 dnf install -y --setopt=install_weak_deps=False python3-cython
 
-wget --tries=5 https://github.com/45Drives/python3-libzfs/archive/refs/tags/v2.4.1-4.tar.gz
+wget --tries=5 https://github.com/bdfd9/python3-libzfs/archive/refs/tags/v2.4.1-4-fork.tar.gz
 
-tar -z -x --no-same-owner --no-same-permissions -f "v2.4.1-4.tar.gz"
+tar -z -x --no-same-owner --no-same-permissions -f "v2.4.1-4-fork.tar.gz"
 
-cd python3-libzfs-2.4.1-4
+cd python3-libzfs-2.4.1-4-fork
 
 ./configure --prefix=/usr && make install
 
-dnf remove -y python3-cython
+cd -
